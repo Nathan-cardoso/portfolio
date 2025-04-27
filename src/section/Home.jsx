3175443ec645e71485b66f07634b5caa7125c127
Nathan-cardoso <nathancar.linhares77@gmail.com>
2025-04-27 11:29:34 -0300

    feat: add background animated

--- src/section/Home.jsx
+++ src/section/Home.jsx
@@ -1,21 +1,27 @@
-    import { TypeAnimation } from "react-type-animation";
+import background from "../assets/images/background.svg";
+import { TypeAnimation } from "react-type-animation";
 
-    function Home() {
-        return (
-            <div id="Home" className="flex flex-col items-center justify-center h-screen px-4">
-                <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center font-[Roboto]">
-                    Olá, eu sou Nathan Cardoso
-                </h2>
-                <h3 className="font-bold text-center mt-4 font-[Ubuntu_Mono] text-xl sm:text-3xl md:text-4xl lg:text-5xl">
-                    <TypeAnimation 
-                        className="text-red-700"
-                        sequence={["Desenvolvedor Back-end"]}
-                        cursor={true}
-                        speed={50}
-                    />
-                </h3>
-            </div>
-        )
-    }
+function Home() {
+    return (
+        <div id="Home" className="relative flex flex-col items-center justify-center h-screen px-4 overflow-hidden">
+            <div
+                className="absolute inset-0 bg-no-repeat bg-cover bg-center opacity-15"
+                style={{ backgroundImage: `url(${background})` }}
+            ></div>
 
-    export default Home;
+            <h2 className="bg-[#0a0a0a] rounded-2xl text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center font-[Roboto] z-10">
+                Olá, eu sou Nathan Cardoso
+            </h2>
+            <h3 className="bg-[#0a0a0a] rounded-2xl font-bold text-center mt-4 font-[Ubuntu_Mono] text-xl sm:text-3xl md:text-4xl lg:text-5xl z-10">
+                <TypeAnimation 
+                    className="text-red-700"
+                    sequence={["Desenvolvedor Back-end"]}
+                    cursor={true}
+                    speed={50}
+                />
+            </h3>
+        </div>
+    )
+}
+
+export default Home;
