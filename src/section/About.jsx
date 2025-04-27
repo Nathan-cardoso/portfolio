4895d7755bb38c9f084790b4ed07d8fbf975688d
Nathan-cardoso <nathancar.linhares77@gmail.com>
2025-04-27 17:46:50 -0300

    feat: add photo profile

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -1,21 +1,18 @@
+import profileImage from '../assets/images/profile.jpeg';
+
+
 function About() {
   return (
-    <div id="About" className="h-screen bg-black ">
-      <h2 className="text-5xl text-center">Sobre Mim</h2>
+    <div id="About" className="h-screen bg-black">
+      <h2 className="text-5xl text-center p-20">SOBRE MIM</h2>
 
-      <div className="text-center flex flex-col items-center justify-center rounded-lg  ">
-        <div className="bg-[#0a0a0a] sm:w-sm md:w-md lg:w-lg">
+      <div className='flex items-center justify-center'>
+        <div className="bg-[#1a1a1a]  rounded-2xl font-[Roboto] text-justify p-5 w-3xl m-[25px] text-lg sm:w-sm sm:text-xl  md:w-xl md:text-2xl lg:w-3xl">
           <p>
-            This is a simple React application that demonstrates the use of
-            components, state management, and routing.
-          </p>
-          <p>
-            The application is built using React and uses React Router for
-            navigation between different sections.
+            Muito prazer, me chamo <span className="text-red-800">Nathan Cardoso</span>. Sou estudante de Tecnologia da Informação pela UFERSA, <em className="text-red-800">Desenvolvedor Back-end</em> e entusiasta a <em className="text-red-800">Pentester</em>. Tenho interesse nas áreas de Desenvolvimente de Software e Cybersegurança.
           </p>
         </div>
       </div>
-
     </div>
   );
 }
